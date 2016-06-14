ff3252cb2a9ea423b1b5bf36bb3f173274333258
mac <2382708330@qq.com>
2016-06-14 17:23:21 +0800

    文字滚动条

---
+++ GYChangeTextViewDemo/GYChangeTextViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GYChangeTextViewDemo
+//
+//  Created by mac on 16/6/14.
+//  Copyright © 2016年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GYChangeTextViewDemo/GYChangeTextViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GYChangeTextViewDemo
+//
+//  Created by mac on 16/6/14.
+//  Copyright © 2016年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
